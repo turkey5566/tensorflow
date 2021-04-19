0a74d219b28d10c6e29a7b47c01cd8809f6781a0
takashiohnuma <turkey5566@hotmail.com>
2021-04-20 00:32:46 +0900

    add new files

---
+++ firstTensorflow.py
@@ -0,0 +1,11 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+# TensorFlow と tf.keras のインポート
+import tensorflow as tf
+from tensorflow import keras
+
+# ヘルパーライブラリのインポート
+import numpy as np
+import matplotlib.pyplot as plt
+
+print(tf.__version__)
